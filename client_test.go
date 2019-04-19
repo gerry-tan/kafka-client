771660601ac7013a5771fb21c6336833e5e61d84
gerry.tan <gerry.tan@blackshark.com>
2019-04-19 16:48:19 +0800

    测试用例优化

--- client_test.go
+++ client_test.go
@@ -2,6 +2,9 @@ package kafka_client
 
 import (
 	"fmt"
+	"github.com/bsm/sarama-cluster"
+	"os"
+	"os/signal"
 	"testing"
 	"time"
 )
@@ -12,21 +15,35 @@ var (
 	groupId = "test1234"
 )
 
+// kafka 生产者
 func TestPutMsg(t *testing.T) {
 	InitProducer(hosts)
 	PutMsg(topic, []byte("hello world!"), "1")
 	time.Sleep(1 * time.Second)
 }
 
+// kafka 消费测试
 func TestNewGroupConsumer(t *testing.T) {
 	group := NewGroupConsumer(hosts, []string{topic}, groupId, nil)
 
-	for pc := range group.Partitions() {
-		go func() {
-			for msg := range pc.Messages() {
-				fmt.Printf("topic: %s, offset: %d, value: %v, key: %v\n", msg.Topic, msg.Offset, string(msg.Value), string(msg.Key))
-				pc.MarkOffset(msg.Offset, "")
-			}
-		}()
+	// Trap SIGINT to trigger a graceful shutdown.
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+
+	for {
+		select {
+		case pc := <-group.Partitions():
+			go handlePartition(pc)
+		case <-signals:
+			CloseConsumer()
+			return
+		}
+	}
+}
+
+func handlePartition(pc cluster.PartitionConsumer) {
+	for msg := range pc.Messages() {
+		fmt.Printf("topic: %s, offset: %d, value: %v, key: %v\n", msg.Topic, msg.Offset, string(msg.Value), string(msg.Key))
+		pc.MarkOffset(msg.Offset, "") //提交
 	}
 }
